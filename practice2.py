1c92e6c48781cc3f5c35905ca78c614b09b77de0
syeda areej <syedaareej789@gmail.com>
2021-03-14 20:32:54 +0500

    first commit

---
+++ practice2.py
@@ -0,0 +1,14 @@
+var1=4                    #int
+var2="25"     #string
+var3=5.7             #float
+var4="20"        #string
+var5=" Areej"    #string
+print(var2)
+print(int(var4)+int(var2)) #add two strings by converting to int
+print(10*"AReej\n")      #to print areej 10 times
+print(var4 + var5)      #this will print 20 areej as both are strings
+print("enter first no")
+num1=input()                        #taking input from user
+print("enter second no")
+num2=input()
+print("sum is",int(num1)+int(num2))
